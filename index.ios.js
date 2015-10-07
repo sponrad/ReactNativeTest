6682e779c0470a73465c115bb21270a624238ef0
Conrad <sponrad@gmail.com>
2015-10-07 10:25:22 -0700

    initial commit started tutorial

---
+++ index.ios.js
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+
+var FirstProject = React.createClass({
+  getInitialState: function() {
+    return {
+      movies: null,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
+
+  render: function() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function(){
+    return(
+      <View style={styles.container}>
+
+      <Text>
+      Loading movies...
+      </Text>
+      
+      </View>
+    );
+  },
+  
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+
+      <Image source={{uri: movie.posters.thumbnail}}
+      style={styles.thumbnail}
+      />
+
+      <View style={styles.rightContainer}>
+      <Text style={styles.title}>{movie.title}</Text>
+      <Text style={styles.year}>{movie.year}</Text>
+      </View>
+
+      </View>
+    );
+  },
+  
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 28,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+});
+
+AppRegistry.registerComponent('FirstProject', () => FirstProject);
